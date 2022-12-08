bdb5af5a3f3693983e093748fd9bc9b0323ede6d
Andika Sujanadi <51147116+andikasujanadi@users.noreply.github.com>
2022-12-08 16:25:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from chat import *
 if __name__ == '__main__':
     start_chat(
         user1='andika',
-        user2='diana',
+        user2='xena',
         scenario='stranger',
         style='roleplay',
     )
